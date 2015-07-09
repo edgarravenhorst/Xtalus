486e17ac48bc3872377e4dcfa8f4243a10831c67
mathijsjansen <mathijs@webineigenhanden.nl>
2015-07-09 13:51:10 +0200

    project page

--- app/models/demand.js
+++ app/models/demand.js
@@ -1,7 +1,19 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+    URI: DS.attr(),
     description: DS.attr(),
+	summary: DS.attr(),
     profiles: DS.attr(),
     owner:DS.belongsTo('person'),
+
+	isisObj:function(){
+        return $ISIS.get('http://acc.xtalus.gedge.nl/simple/restful/'+this.get('URI')).then(function(isisObjData){
+
+		console.log($ISIS.extractMembers(isisObjData));
+            return ($ISIS.extractMembers(isisObjData));
+
+        });
+    }.property('URI')
+
 });
