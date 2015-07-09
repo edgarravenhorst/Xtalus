e3c5c95e443afebf03e67c07d3386e45f6bbde90
mathijsjansen <mathijs@webineigenhanden.nl>
2015-07-09 12:49:51 +0200

    cleanup template

--- app/routes/application.js
+++ app/routes/application.js
@@ -40,11 +40,6 @@ var ApplicationRoute = Ember.Route.extend({
             this.transitionTo('profile', id);
         },
 
-        toggleAccountNav:function(){
-            Ember.$('body').toggleClass('account-nav-visible');
-            return false;
-        },
-
         toggleUsernav:function(){
             Ember.$('body').toggleClass('user-nav-small');
             return false;
